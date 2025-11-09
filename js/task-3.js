f0ca8bc38e980ae43723e4326fe9c963de7e3df8
Miyazmm <alinakulichenko1@gmail.com>
2025-11-09 13:55:54 +0200

    comment-removal

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,5 @@
 function filterArray(numbers, value) {
     const newArray = [];
-    // for (let i = 0; i < numbers.length; i += 1){
-    //     if (numbers[i] > value) {
-    //         newArray.push(numbers[i]);
-    //     }
-    // }
     for (const element of numbers) {
         if (element > value) {
             newArray.push(element);
